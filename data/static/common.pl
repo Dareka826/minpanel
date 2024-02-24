e69e99b63f601ac4daafa97cfddf8eb7674a22f1
Rin Brk <dareka826@gmail.com>
2024-02-24 11:25:47 +0100

    common.pl: - Add printl - Add escape_spaces - Remove script tag after setting title - Add allowed_methods - Add e & ea - Add print_headers - Add global $br

--- data/static/common.pl
+++ data/static/common.pl
@@ -2,6 +2,13 @@ our $NL = "\r\n";
 
 require '../static/spawn.pl';
 
+sub printl {
+    for my $arg (@_) {
+        print($arg);
+    }
+    print("\n");
+}
+
 sub escape_html_special {
     my ($str) = @_;
 
@@ -11,6 +18,13 @@ sub escape_html_special {
     $str =~ s/</&#60;/g;
     $str =~ s/>/&#62;/g;
 
+    return $str;
+}
+*text = \&escape_html_special;
+
+sub escape_spaces {
+    my ($str) = @_;
+    $str =~ s/ /\&nbsp;/g;
     return $str;
 }
 
@@ -57,17 +71,84 @@ sub parse_url_params {
 
 sub set_title {
     my ($title) = @_;
+    $title = escape_html_special($title);
 
     return <<"EOF";
-<script type="text/javascript">
+<script id="set-title-script" type="text/javascript">
+    // Add title element if missing
     if (document.querySelector("head > title") === null) {
         let t = document.createElement("title");
         document.head.appendChild(t);
     }
 
+    // Set title content
     document.querySelector("head > title").innerHTML = '$title';
+
+    // Remove this script tag
+    let me = document.querySelector("#set-title-script");
+    me.parentElement.removeChild(me);
 </script>
 EOF
 }
+
+sub allowed_methods {
+    my ($arrref_methods) = @_;
+    my @methods = @{$arrref_methods};
+    my $env_method = $ENV{"REQUEST_METHOD"};
+
+    for my $method (@methods) {
+        if ($method eq $env_method) {
+            return;
+        }
+    }
+    exit(1);
+}
+
+sub ea {
+    my ($tag, $attributes_hashref, $content) = @_;
+
+    # attributes hash to text
+    my $attributes_txt = "";
+    if (defined($attributes_hashref)) {
+        my %attributes = %{$attributes_hashref};
+
+        for my $attr (keys %attributes) {
+            my $val = $attributes{$attr};
+
+            if (!defined($val)) {
+                $attributes_txt = $attributes_txt . " $attr";
+                next;
+            }
+
+            $val =~ s/"/\\"/g;
+            $attributes_txt = $attributes_txt . " $attr=\"$val\"";
+        }
+    }
+
+    if (!defined($content)) {
+        return "<$tag$attributes_txt/>";
+    }
+    return "<$tag$attributes_txt>$content</$tag>";
+}
+
+sub e {
+    my ($tag, $content) = @_;
+    return ea($tag, undef, $content);
+}
+
+sub print_headers {
+    my ($headers_hashref) = @_;
+    if (!defined($headers_hashref)) { return; }
+
+    my %headers = %{$headers_hashref};
+    for my $key (keys %headers) {
+        my $val = $headers{$key};
+
+        printl("$key: $val");
+    }
+    printl();
+}
+
+our $br = e("br", undef);
 
 1;
